52e5b1c7cb8c87adf470deea96494b0dc6c063b6
Connor O'Rourke <connororourke25@gmail.com>
2019-10-30 00:44:11 -0500

    hello world

--- App.js
+++ App.js
@@ -9,10 +9,8 @@ const instructions = Platform.select({
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{flex: 1, justifyContent:"center", alignItems:"center"}}>
+        <Text>Hello, World!</Text>
       </View>
     );
   }
