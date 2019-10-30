6090110b9aa6a6d2c135a64e58cb72090451ae52
Connor O'Rourke <connororourke25@gmail.com>
2019-10-30 01:32:20 -0500

    added pictures

--- App.js
+++ App.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-class Greeting extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center'}}>
-        <Text>Hello {this.props.name}!</Text>
-      </View>
-    );
+class Blink extends Component {
+  componentDidMount() {
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText}
+      ), 1000);
+    )
   }
-}
 
-export default class LotsOfGreetings extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center', top: 50}}>
-        <Greeting name='Rexxar'/>
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
-      </View>
-    );
+    state = { isShowingText: true};
+
+    render() {
+      if(!this.state.isShowingText) {
+        return null;
+      }
+
+      return (
+        <Text>{this.props.text}</text>
+      );
+    }
   }
-}
+
+  export default class BlinkApp extends Component {
+    render() {
+      return (
+        <View>
+          <Blink text='I love to blink'/>
+          <Blink text='Yes blinking is so great'/>
+          <Blink text='Why did they ever take this out of HTML'/>
+          <Blink text='Look at me look at me look at me'/>
+        </View>
+      );
+    }
+  }
